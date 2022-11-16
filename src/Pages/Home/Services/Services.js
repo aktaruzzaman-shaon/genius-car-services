16dcb1904bdea14887e6bc15e1e034334ae30942
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2022-11-16 20:02:37 +0600

    adding id of specific parts and dynamic route

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
 
 
     return (
-        <div className='container'>
+        <div id='services' className='container'>
             <div className="row">
                 <h1 className='services-title m-5'> Our Services </h1>
                 <div className='services-container'>
