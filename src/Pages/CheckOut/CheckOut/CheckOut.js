3bdec07d79de2e531d672d0b6c20894511ceebaf
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2022-11-19 19:12:51 +0600

    uddating authentication

---
+++ src/Pages/CheckOut/CheckOut/CheckOut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckOut = () => {
+    return (
+        <div>
+            <h1>Plz check out your booking</h1>
+        </div>
+    );
+};
+
+export default CheckOut;
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 import './Register.css'
+import auth from '../../../firebase.init';
 const Register = () => {
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
     const navigate = useNavigate();
     const navigateLogin = ()=>{
         navigate('/login')
     }
+
+    if(user){
+        navigate('/home');
+    }
+
     const handleRegister = event =>{
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
     }
+
     return (
         <div className='register-form'>
             <h2 style={{textAlign: "ceneter"}}>Plz register</h2>
+
             <form onSubmit={handleRegister}>
                 <input type="text" name="name" placeholder='Your Name'/>
                 <input type="email" name='email' placeholder='Email Address' required />
                 <input type="password" name='password' placeholder='Password' required/>
                 <input type="submit" value="Register" />
             </form>
+
             <p>Already have an account ? <Link className='text-danger pe-auto text-decoration-none' to={'/login'} onClick={navigateLogin}>Login</Link></p>
         </div>
     );
